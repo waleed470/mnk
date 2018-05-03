043d4e1935a9f9749bad1530cdbebfa36c015624
honey <honey@DESKTOP-K6NUG09>
2018-05-03 14:43:00 +0500

    uploading admin data

---
+++ Mnk/Mnk/Controllers/adminhomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mnk.Controllers
+{
+    public class adminhomeController : Controller
+    {
+        // GET: adminhome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
